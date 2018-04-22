e368ed013a1bf2ec3cc6d2c2c998e668fa2c6a0f
Laura Hernández <LauraHernandez@lauras-macbook-pro-2.local>
2018-04-21 21:23:43 -0700

    Can print playable and real, checks and prints places for adjacent

--- MineSweeper Part 2/main.cpp
+++ MineSweeper Part 2/main.cpp
@@ -9,12 +9,17 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <vector>
+
 using namespace std;
 class Board{
 private:
     int size;
     int mines;
-    char matrix[25][25];
+    char real[25][25];
+    char playable[25][25];
+    char adjacent[25][25];
+    int minesList[100][2];
 public:
     Board(string difficulty){
         if(difficulty == "Beginner")
@@ -32,55 +37,88 @@ public:
             mines = 100;
         }
     }
-    void createBoard()
+    void createBoard(bool realBoard)
     {
         //fills the board with points
         for (int j = 0; j<size; j++)
         {
             for (int i = 0; i<size; i++)
             {
-                matrix[i][j] = '.';
+                real[i][j] = '.';
+                playable[i][j] = '.';
                 
             }
         }
         //fills the board with the bombs
-        int rnumb= 0;
-        int rnumb2= 0;
-        for(int i= 0; i<mines;i++){
-            rnumb = rand() % size;
-            rnumb2= rand() % size;
-            if(rnumb == 0)
-                rnumb++;
-            if (rnumb2 == 0)
-                rnumb2++;
-            if (matrix[rnumb][rnumb2] != '*') {
-                matrix[rnumb][rnumb2] = '*';
-            }else i--;
+        if (realBoard == true) {
+            int rnumb= 0;
+            int rnumb2= 0;
+            for(int i= 0; i<mines;i++){
+                rnumb = rand() % size;
+                rnumb2= rand() % size;
+                minesList[i][0] = rnumb;
+                minesList[i][1] = rnumb2;
+                if (real[rnumb][rnumb2] != '*') {
+                    real[rnumb][rnumb2] = '*';
+                }else i--;
+            }
         }
+        //fills the board with numbers
+        
+        //copies the boards
+        for (int i = 0; i<size; i++)
+        {
+            for (int j = 0; j<size; j++) {
+                adjacent[i][j] = real[i][j];
+            }
+        }
+        
     }
+        
     
-    void printBoard()
+    void printBoard(bool realBoard)
     {
         cout<<"  ";
-        for (int z = 1; z<size+1; z++) {
-            cout << setw(2)<< (char)(z+64);
+        for (int z = 0; z<size; z++) {
+            cout << setw(2)<< z;
         }
         cout<<endl;
-        
-        for(int y=0;y<size;y++)
+        if (realBoard == true)
         {
-            cout<<setw(2)<<y+1;
-            for(int x=0;x<size;x++)
+            for(int y=0;y<size;y++)
             {
-                cout << setw(2)<< matrix[x][y];
+                cout<<setw(2)<<y;
+                for(int x=0;x<size;x++)
+                {
+                    cout << setw(2)<< real[x][y];
+                }
+
+                cout<<endl;
             }
             cout<<endl;
+            for(int y=0;y<2;y++)
+            {
+                for(int x=0;x<mines;x++)
+                {
+                    cout << setw(2)<< minesList[x][y];
+                }
+                
+                cout<<endl;
+            }
         }
-        
+        else if (realBoard == false)
+        {
+            for(int y=0;y<size;y++)
+            {
+                cout<<setw(2)<<y+1;
+                for(int x=0;x<size;x++)
+                {
+                    cout << setw(2)<< playable[x][y];
+                }
+                cout<<endl;
+            }
+        }
     }
-    
-    
-    
 };
 
 int main() {
@@ -89,23 +127,23 @@ int main() {
     cout <<"Please select a level: 1 for Beginner, 2 for Intermediate and 3 for Advanced"<<endl;
     cin >> level;
     
-    if (level == 1){
+    if (level == 1)
+    {
         Board vboard("Beginner");
-        vboard.createBoard();
-        vboard.printBoard();
-        
-    }else if (level == 2){
-        Board v2board("Intermediate");
-        v2board.createBoard();
-        v2board.printBoard();
-    }else if (level == 3){
-        Board v3board("Advanced");
-        v3board.createBoard();
-        v3board.printBoard();
+        vboard.createBoard(true);
+        vboard.printBoard(true);
     }
-    
-    
-    
-    
+    else if (level == 2)
+    {
+        Board v2board("Intermediate");
+        v2board.createBoard(true);
+        v2board.printBoard(true);
+    }
+    else if (level == 3)
+    {
+        Board v3board("Advanced");
+        v3board.createBoard(true);
+        v3board.printBoard(true);
+    }
     return 0;
 }
