9ad10c0f8159d819580cad9e38b6b15f1270362c
Laura Hernandez <LauraHernandez@lauras-macbook-pro-2.local>
2018-09-12 13:45:26 -0700

    final

--- MineSweeper Part 2/main.cpp
+++ MineSweeper Part 2/main.cpp
@@ -1,259 +1,270 @@
-//
-//  main.cpp
-//  MineSweeper Part 2
-//
-//  Created by Laura Hernandez on 4/21/18.
-//  Copyright © 2018 Laura Hernandez. All rights reserved.
-//
+    //
+    //  main.cpp
+    //  MineSweeper Part 2
+    //
+    //  Created by Laura Hernandez on 4/21/18.
+    //  Copyright © 2018 Laura Hernandez. All rights reserved.
+    //
 
-#include <iostream>
-#include <string>
-#include <iomanip>
-#include <vector>
+    #include <iostream>
+    #include <string>
+    #include <iomanip>
+    #include <vector>
 
-using namespace std;
-class Board{
-private:
-    int size;
-    int mines;
-    char real[25][25];
-    char playable[25][25];
-    char adjacent[25][25];
-    int minesList[100][2];
-    int counter = 0;
-    int total = 0;
-    int zeros[100][25];
-public:
-    Board(string difficulty)
-    {
-        if(difficulty == "Beginner")
+    using namespace std;
+    class Board{
+    private:
+        int size;
+        int mines;
+        char real[25][25];
+        char playable[25][25];
+        char adjacent[25][25];
+        int minesList[100][2];
+        int counter = 0;
+        int total = 0;
+        int zeros[100][25];
+    public:
+        Board(string difficulty)
         {
-            size = 9;
-            mines = 10;
-            
-        }else if (difficulty == "Intermediate")
-        {
-            size = 16;
-            mines = 40;
-        } else if(difficulty == "Advanced")
-        {
-            size = 24;
-            mines = 100;
-        }
-    }
-    
-    void createBoards()
-    {
-        //fills the real and playable board with points and adjacent with zeros
-        for (int j = 0; j<size; j++)
-        {
-            for (int i = 0; i<size; i++)
+            if(difficulty == "Debug"){
+                size = 3;
+                mines = 1;
+            }
+            else if(difficulty == "Beginner")
             {
-                real[i][j] = '.';
-                playable[i][j] = '.';
-                adjacent[i][j] = '0';
+                size = 9;
+                mines = 10;
+                
+            }else if (difficulty == "Intermediate")
+            {
+                size = 16;
+                mines = 40;
+            } else if(difficulty == "Advanced")
+            {
+                size = 24;
+                mines = 100;
+            }
+        }
+        
+        void createBoards()
+        {
+            //fills the real and playable board with points and adjacent with zeros
+            for (int j = 0; j<size; j++)
+            {
+                for (int i = 0; i<size; i++)
+                {
+                    real[i][j] = '.';
+                    playable[i][j] = '.';
+                    adjacent[i][j] = '0';
+                    
+                }
+            }
+            //fills the real board and the adjacent board with the bombs
+                int rnumb= 0;
+                int rnumb2= 0;
+            for(int i= 0; i<mines;i++)
+            {
+                rnumb = rand() % size;
+                rnumb2= rand() % size;
+                minesList[i][0] = rnumb;
+                minesList[i][1] = rnumb2;
+                if (real[rnumb][rnumb2] != '*')
+                {
+                    real[rnumb][rnumb2] = '*';
+                    adjacent[rnumb][rnumb2] = '*';
+                }
+                else i--;
+            }
+            //fills adjacent with numbers
+            for (int i = 0; i<mines; i++)
+            {
+                int x = (minesList[i][0]);
+                int y =(minesList[i][1]);
+                char charT = adjacent[x][y];
+                
+                for (int posx = -1; posx < 2; posx++)
+                {
+                    for (int posy = -1; posy < 2; posy++)
+                    {
+                        charT = adjacent[x + posx][y + posy];
+                        if (charT != '*')
+                        {
+                        adjacent[x + posx][y + posy] = charT + 1;
+                            
+                        }
+                    }
+                }
                 
             }
         }
-        //fills the real board and the adjacent board with the bombs
-            int rnumb= 0;
-            int rnumb2= 0;
-        for(int i= 0; i<mines;i++)
-        {
-            rnumb = rand() % size;
-            rnumb2= rand() % size;
-            minesList[i][0] = rnumb;
-            minesList[i][1] = rnumb2;
-            if (real[rnumb][rnumb2] != '*')
-            {
-                real[rnumb][rnumb2] = '*';
-                adjacent[rnumb][rnumb2] = '*';
-            }
-            else i--;
-        }
-        //fills adjacent with numbers
-        for (int i = 0; i<mines; i++)
-        {
-            int x = (minesList[i][0]);
-            int y =(minesList[i][1]);
-            char charT = adjacent[x][y];
-            
-            for (int posx = -1; posx < 2; posx++)
-            {
-                for (int posy = -1; posy < 2; posy++)
-                {
-                    charT = adjacent[x + posx][y + posy];
-                    if (charT != '*')
-                    {
-                    adjacent[x + posx][y + posy] = charT + 1;
-                        
-                    }
-                }
-            }
-            
-        }
-    }
         
-    
-    void printBoard(string type)
-    {
-        // prints number up
-        cout<<endl;
-        cout<<"   ";
-        for (int z = 0; z<size; z++) {
-            cout << setw(3)<< z;
-        }
-        cout<<endl;
-        if (type == "real")
+        
+        void printBoard(string type)
         {
-        // prints real board
-            for(int y=0;y<size;y++)
-            {
-                //prints the numbers down
-                cout<<setw(3)<<y;
-                for(int x=0;x<size;x++)
-                {
-                    cout << setw(3)<< real[x][y];
-                }
-                cout<<endl;
+            // prints number up
+            cout<<endl;
+            cout<<"   ";
+            for (int z = 0; z<size; z++) {
+                cout << setw(3)<< z;
             }
-        }
-        else if (type == "adjacent")
-        {
-        //print adjacent
+            cout<<endl;
+            if (type == "real")
+            {
+            // prints real board
                 for(int y=0;y<size;y++)
                 {
                     //prints the numbers down
                     cout<<setw(3)<<y;
                     for(int x=0;x<size;x++)
                     {
-                        cout << setw(3)<< adjacent[x][y];
+                        cout << setw(3)<< real[x][y];
                     }
                     cout<<endl;
                 }
             }
-
-        // prints playable
-        else if (type == "playable")
-        {
-            for(int y=0;y<size;y++)
+            else if (type == "adjacent")
             {
-                cout<<setw(3)<<y;
-                for(int x=0;x<size;x++)
-                {
-                    cout << setw(3)<< playable[x][y];
+            //print adjacent
+                    for(int y=0;y<size;y++)
+                    {
+                        //prints the numbers down
+                        cout<<setw(3)<<y;
+                        for(int x=0;x<size;x++)
+                        {
+                            cout << setw(3)<< adjacent[x][y];
+                        }
+                        cout<<endl;
+                    }
                 }
+
+            // prints playable
+            else if (type == "playable")
+            {
+                for(int y=0;y<size;y++)
+                {
+                    cout<<setw(3)<<y;
+                    for(int x=0;x<size;x++)
+                    {
+                        cout << setw(3)<< playable[x][y];
+                    }
+                    cout<<endl;
+                }
+            }
+        }
+        void printMinesList(){
+            //prints the number of the mines list
+            cout<<endl;
+            for(int y=0;y<2;y++)
+            {
+                for(int x=0;x<mines;x++)
+                {
+                    cout << setw(2)<< minesList[x][y];
+                }
+                
                 cout<<endl;
             }
         }
-    }
-    void printMinesList(){
-        //prints the number of the mines list
-        cout<<endl;
-        for(int y=0;y<2;y++)
+        // merges playable and adjacent to make the game playable
+        int reveal(int x, int y)
         {
-            for(int x=0;x<mines;x++)
+            total =(size*size) - mines;
+            //if they don't hit a mine
+            if (adjacent[x][y] != '*')
             {
-                cout << setw(2)<< minesList[x][y];
-            }
-            
-            cout<<endl;
-        }
-    }
-    // merges playable and adjacent to make the game playable
-    int reveal(int x, int y)
-    {
-        total =(size*size) - mines;
-        //if they don't hit a mine
-        if (adjacent[x][y] != '*')
-        {
-            if (playable[x][y] == '.')
-            {
-                counter++;
-                playable[x][y] = adjacent[x][y];
-                if (adjacent[x][y] == '0')
+                if (playable[x][y] == '.')
                 {
-                    for (int posx = -1; posx < 2; posx++)
+                    playable[x][y] = adjacent[x][y];
+                    counter++;
+                    if (adjacent[x][y] == '0')
                     {
-                        for (int posy = -1; posy < 2; posy++)
+                        for (int posx = -1; posx < 2; posx++)
                         {
-                            if(adjacent[x + posx][y + posy] == '0')
+                            for (int posy = -1; posy < 2; posy++)
                             {
-                                reveal((x+posx), (y+posy));
+                                if(adjacent[x + posx][y + posy] == '0' && playable[x + posx][y + posy] == '.' )
+                                {
+                                    reveal((x+posx), (y+posy));
+                                }
+                                if(playable[x + posx][y + posy] == '.'){
+                                    playable[x+ posx][y+posy] = adjacent[x + posx][y + posy];
+                                    counter++;
+                                }
                             }
-                            playable[x+ posx][y+posy] = adjacent[x + posx][y + posy];
-                            counter++;
                         }
                     }
                 }
-            }
-            if (counter == total)
-            {
-                return 1;
-            }
-        }
-        // if they hit a mine
-        else
-        {
-            for(int i=0;i<size;i++)
-            {
-                for(int j=0;j<size;j++)
+                if (counter == total)
                 {
-                    playable[i][j] = adjacent[i][j];
+                    return 1;
                 }
-                cout<<endl;
             }
-            cout << "Sorry! Better luck next time!";
-            return 0;
+            // if they hit a mine
+            else
+            {
+                for(int i=0;i<size;i++)
+                {
+                    for(int j=0;j<size;j++)
+                    {
+                        playable[i][j] = adjacent[i][j];
+                    }
+                    cout<<endl;
+                }
+                cout << "Sorry! Better luck next time!";
+                return 0;
+            }
+            cout<<counter<<endl;
+            return 2;
         }
-        return 2;
-    }
-};
-Board test("Beginner");
-int main() {
-    srand(static_cast<int>(time(0)));
-    int level;
-    cout <<"Please select a level: 1 for Beginner, 2 for Intermediate and 3 for Advanced"<<endl;
-    cin >> level;
-    int won;
-    
-    // creates the board depending on the level selected
-    string levelS;
-    if (level == 1)
-    {
-        levelS ="Beginner";
-    }
-    else if (level == 2)
-    {
-        levelS = "Intermediate";
-    }
-    else if (level == 3)
-    {
-        levelS = "Advanced";
-    }
-    Board vboard(levelS);
-    vboard.createBoards();
-    test =vboard;
-    // prints instructions on how to play and the playable board
-    cout<<"To play, please enter the box you want to press in the order of ACROSS hit space and DOWN, to restart type 99 99";
-    int accross =0;
-    int down = 0;
-    vboard.printBoard("playable");
-    do
-    {
-        cout << "input: ";
-        cin >> accross >> down;
-        won = vboard.reveal(accross, down);
+    };
+    Board test("Beginner");
+    int main() {
+        srand(static_cast<int>(time(0)));
+        int level;
+        cout <<"Please select a level: 1 for Beginner, 2 for Intermediate and 3 for Advanced"<<endl;
+        cin >> level;
+        int won;
+        
+        // creates the board depending on the level selected
+        string levelS;
+        if(level == 0){
+            levelS ="Debug";
+        }
+        else if (level == 1)
+        {
+            levelS ="Beginner";
+        }
+        else if (level == 2)
+        {
+            levelS = "Intermediate";
+        }
+        else if (level == 3)
+        {
+            levelS = "Advanced";
+        }
+        Board vboard(levelS);
+        vboard.createBoards();
+        test =vboard;
+        // prints instructions on how to play and the playable board
+        cout<<"To play, please enter the box you want to press in the order of ACROSS hit space and DOWN, to restart type 99 99";
+        int accross =0;
+        int down = 0;
         vboard.printBoard("playable");
+        do
+        {
+            cout << "input: ";
+            cin >> accross >> down;
+            won = vboard.reveal(accross, down);
+            vboard.printBoard("playable");
+        }
+        while (won == 2);
+        if (won == 1){
+            //system("afplay owen_wilson_wow.m4a");
+            cout<<"CONGRTULATIONS, YOU'VE WON"<<endl;
+        }
+        else if (won == 0){
+            cout <<"Better luck next time!"<<endl;
+        }
+        cout << "Thank you for playing!"<< endl;
+        
+        return 0;
     }
-    while (won == 2);
-    if (won == 1){
-        cout<<"CONGRTULATIONS, YOU'VE WON"<<endl;
-    }
-    else if (won == 0){
-        cout <<"Better luck next time!";
-    }
-    cout << "Thank you for playing!"<< endl;
-    
-    return 0;
-}
